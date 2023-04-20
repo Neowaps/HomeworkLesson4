c5660d21367ba8dcea3f3151a7dd1e7357a6e2e6
Neowaps <Neowapsn1@mail.ru>
2023-04-20 19:57:13 +0300

    complete Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+string textN = Console.ReadLine();
+int number = int.Parse(textN);
+Console.Write("Введите степень, в которую необходимо возвести число: ");
+string textD = Console.ReadLine();
+int degree = int.Parse(textD);
+
+double result = Math.Pow(number, degree);
+Console.Write($"Результат возведения в степень: {result}");
