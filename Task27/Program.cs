5471ce54c4dc05d1a779d0b5f29984e464c27c9b
Neowaps <Neowapsn1@mail.ru>
2023-04-23 12:12:35 +0300

    complete Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿int ConvertToNumber (string textMessage)
+{
+    System.Console.Write(textMessage);
+    string textNumber = Console.ReadLine();
+    int convertNumber = int.Parse(textNumber);
+    return convertNumber;
+}
+
+int GetSumNumbers (int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+int inputNumber = ConvertToNumber("Введите число: ");
+System.Console.WriteLine($"Сумма чисел в цифре {inputNumber} равна: {GetSumNumbers(inputNumber)}");
